bfbbab2a3e584faadbdf663ccdcfad1f87f346ae
Jason Rey <jasonrey@outlook.com>
2015-07-21 09:05:54 +0800

    Updated routing method

--- lib.php
+++ lib.php
@@ -7,265 +7,273 @@ require_once('req.php');
 
 class Lib
 {
-    public static function ajax()
-    {
-        static $loaded;
+	public static function ajax()
+	{
+		static $loaded;
 
-        if (empty($loaded)) {
-            require_once(dirname(__FILE__) . '/ajax.php');
+		if (empty($loaded)) {
+			require_once(dirname(__FILE__) . '/ajax.php');
 
-            $loaded = true;
+			$loaded = true;
+		}
+
+		return Ajax::init();
+	}
+
+	public static function db()
+	{
+		static $loaded;
+
+		if (empty($loaded)) {
+			require_once(dirname(__FILE__) . '/database.php');
+
+			$loaded = true;
+		}
+
+		return Database::init();
+	}
+
+	public static function view($name)
+	{
+		static $loaded;
+		static $viewsLoaded = array();
+
+		if (empty($loaded)) {
+			require_once(dirname(__FILE__) . '/view.php');
+
+			$loaded = true;
+		}
+
+		if (!in_array($name, $viewsLoaded)) {
+
+			$file = dirname(__FILE__) . '/../views/' . $name . '.php';
+
+			if (file_exists($file)) {
+				require_once($file);
+			}
+
+			$viewsLoaded[] = $name;
+		}
+
+		$classname = ucfirst($name) . 'View';
+
+		if (!class_exists($classname)) {
+			$classname = 'View';
+		}
+
+		$view = new $classname;
+
+		$view->viewname = $name;
+
+		return $view;
+	}
+
+	public static function output($namespace, $vars = array())
+	{
+		$segments = explode('/', $namespace);
+		$view = array_shift($segments);
+		$path = implode('/', $segments);
+
+		$class = Lib::view($view);
+
+		$class->set($vars);
+
+		return $class->output($path);
+	}
+
+	public static function model($name)
+	{
+		static $loaded;
+		static $modelsLoaded = array();
+
+		if (empty($loaded)) {
+			require_once(dirname(__FILE__) . '/model.php');
+
+			$loaded = true;
+		}
+
+		if (empty($name)) {
+			$model = new Model;
+
+			return $model;
+		}
+
+		if (empty($modelsLoaded[$name])) {
+			require_once(dirname(__FILE__) . '/../models/' . $name . '.php');
+
+			$classname = ucfirst($name) . 'Model';
+
+			$model = new $classname;
+
+			$model->tablename = $name;
+
+			$modelsLoaded[$name] = $model;
+		}
+
+		return $modelsLoaded[$name];
+	}
+
+	public static function table($name)
+	{
+		static $loaded;
+		static $tablesLoaded = array();
+
+		if (empty($loaded)) {
+			require_once(dirname(__FILE__) . '/table.php');
+
+			$loaded = true;
+		}
+
+		if (!in_array($name, $tablesLoaded)) {
+			require_once(dirname(__FILE__) . '/../tables/' . $name . '.php');
+
+			$tablesLoaded[] = $name;
+		}
+
+		$classname = ucfirst($name) . 'Table';
+
+		$table = new $classname;
+
+		$table->tablename = $name;
+
+		return $table;
+	}
+
+	public static function route()
+	{
+		 $requesturi = trim(str_replace(Config::getBaseFolder(), '', $_SERVER['REQUEST_URI']), '/');
+
+        if (empty($requesturi)) {
+            $requesturi = 'index';
         }
 
-        return Ajax::init();
-    }
+        $requestSegments = explode('?', $requesturi);
 
-    public static function db()
-    {
-        static $loaded;
+        $base = $requestSegments[0];
 
-        if (empty($loaded)) {
-            require_once(dirname(__FILE__) . '/database.php');
-
-            $loaded = true;
+        if (empty($base)) {
+            $base = 'index';
         }
 
-        return Database::init();
-    }
+        $segments = explode('/', $base);
 
-    public static function view($name)
-    {
-        static $loaded;
-        static $viewsLoaded = array();
+		$key = strtolower(array_shift($segments));
 
-        if (empty($loaded)) {
-            require_once(dirname(__FILE__) . '/view.php');
+		if ($key === 'index.php') {
+			$key = 'index';
+		}
 
-            $loaded = true;
-        }
+		$router = Lib::router($key);
 
-        if (!in_array($name, $viewsLoaded)) {
+		if ($router === false) {
+			// Require error
+			return true;
+		}
 
-            $file = dirname(__FILE__) . '/../views/' . $name . '.php';
+		$result = $router->route($segments);
 
-            if (file_exists($file)) {
-                require_once($file);
-            }
+		if ($result === false) {
+			// Require error
+			return true;
+		}
 
-            $viewsLoaded[] = $name;
-        }
+		return true;
+	}
 
-        $classname = ucfirst($name) . 'View';
+	public static function router($name)
+	{
+		static $loaded;
+		static $loadedRouters;
 
-        if (!class_exists($classname)) {
-            $classname = 'View';
-        }
+		if (!isset($loaded)) {
+			require_once(dirname(__FILE__) . '/router.php');
+			$loaded = true;
+		}
 
-        $view = new $classname;
+		$key = trim(strtolower(preg_replace('/[' . preg_quote('-_', '/') . ']/', '', $name)));
 
-        $view->viewname = $name;
+		if (!isset($loadedRouters[$key])) {
+			$file = dirname(__FILE__) . '/../routers/' . $key . '.php';
 
-        return $view;
-    }
+			if (!file_exists($file)) {
+				return false;
+			}
 
-    public static function output($namespace, $vars = array())
-    {
-        $segments = explode('/', $namespace);
-        $view = array_shift($segments);
-        $path = implode('/', $segments);
+			require_once($file);
 
-        $class = Lib::view($view);
+			$classname = ucfirst($key) . 'Router';
 
-        $class->set($vars);
+			$loadedRouters[$key] = new $classname;
 
-        return $class->output($path);
-    }
+			$loadedRouters[$key]->base = strtolower($name);
+		}
 
-    public static function model($name)
-    {
-        static $loaded;
-        static $modelsLoaded = array();
+		return $loadedRouters[$key];
+	}
 
-        if (empty($loaded)) {
-            require_once(dirname(__FILE__) . '/model.php');
+	public static function url($target, $options = array(), $external = false)
+	{
+		$values = array();
 
-            $loaded = true;
-        }
+		$router = Lib::router($target);
 
-        if (empty($name)) {
-            $model = new Model;
+		$base = $external ? Config::getBaseUrl() . '/' . Config::getBaseFolder() . '/' : '';
 
-            return $model;
-        }
+		if (!$router) {
+			foreach ($options as $k => $v) {
+				$values[] = $k . '=' . $v;
+			}
 
-        if (empty($modelsLoaded[$name])) {
-            require_once(dirname(__FILE__) . '/../models/' . $name . '.php');
+			$queries = implode('&', $values);
 
-            $classname = ucfirst($name) . 'Model';
+			if (!empty($queries)) {
+				$queries = '?' . $queries;
+			}
 
-            $model = new $classname;
+			return $base . $target . '.php' . $queries;
+		}
 
-            $model->tablename = $name;
+		$link = $base . $router->build($options);
 
-            $modelsLoaded[$name] = $model;
-        }
+		return $link;
+	}
 
-        return $modelsLoaded[$name];
-    }
+	public static function session()
+	{
+		static $loaded;
 
-    public static function table($name)
-    {
-        static $loaded;
-        static $tablesLoaded = array();
+		if (empty($loaded)) {
+			require_once(dirname(__FILE__) . '/session.php');
 
-        if (empty($loaded)) {
-            require_once(dirname(__FILE__) . '/table.php');
+			$loaded = true;
+		}
 
-            $loaded = true;
-        }
+		return Session::init();
+	}
 
-        if (!in_array($name, $tablesLoaded)) {
-            require_once(dirname(__FILE__) . '/../tables/' . $name . '.php');
+	public static function cookie()
+	{
+		static $loaded;
 
-            $tablesLoaded[] = $name;
-        }
+		if (empty($loaded)) {
+			require_once(dirname(__FILE__) . '/cookie.php');
 
-        $classname = ucfirst($name) . 'Table';
+			$loaded = true;
+		}
 
-        $table = new $classname;
+		return Cookie::init();
+	}
 
-        $table->tablename = $name;
+	public static function escape($output, $fromUrl = false)
+	{
+		$string = htmlspecialchars($output, ENT_COMPAT, 'UTF-8');
 
-        return $table;
-    }
+		if ($fromUrl) {
+			$string = urlencode($string);
+		}
 
-    public static function route()
-    {
-        $request = rtrim(str_replace(Config::getBaseFolder(), '', $_SERVER['REQUEST_URI']), '/');
-
-        if (empty($request)) {
-            $request = 'index';
-        }
-
-        $segments = explode('/', $request);
-
-        $key = strtolower(array_shift($segments));
-
-        if ($key === 'index.php') {
-            $key = 'index';
-        }
-
-        $router = Lib::router($key);
-
-        if ($router === false) {
-            // Require error
-            return true;
-        }
-
-        $result = $router->route($segments);
-
-        if ($result === false) {
-            // Require error
-            return true;
-        }
-
-        return true;
-    }
-
-    public static function router($name)
-    {
-        static $loaded;
-        static $loadedRouters;
-
-        if (!isset($loaded)) {
-            require_once(dirname(__FILE__) . '/router.php');
-            $loaded = true;
-        }
-
-        $key = trim(strtolower(preg_replace('/[' . preg_quote('-_', '/') . ']/', '', $name)));
-
-        if (!isset($loadedRouters[$key])) {
-            $file = dirname(__FILE__) . '/../routers/' . $key . '.php';
-
-            if (!file_exists($file)) {
-                return false;
-            }
-
-            require_once($file);
-
-            $classname = ucfirst($key) . 'Router';
-
-            $loadedRouters[$key] = new $classname;
-
-            $loadedRouters[$key]->base = strtolower($name);
-        }
-
-        return $loadedRouters[$key];
-    }
-
-    public static function url($target, $options = array(), $external = false)
-    {
-        $values = array();
-
-        $router = Lib::router($target);
-
-        $base = $external ? Config::getBaseUrl() . '/' . Config::getBaseFolder() . '/' : '';
-
-        if (!$router) {
-            foreach ($options as $k => $v) {
-                $values[] = $k . '=' . $v;
-            }
-
-            $queries = implode('&', $values);
-
-            if (!empty($queries)) {
-                $queries = '?' . $queries;
-            }
-
-            return $base . $target . '.php' . $queries;
-        }
-
-        $link = $base . $router->build($options);
-
-        return $link;
-    }
-
-    public static function session()
-    {
-        static $loaded;
-
-        if (empty($loaded)) {
-            require_once(dirname(__FILE__) . '/session.php');
-
-            $loaded = true;
-        }
-
-        return Session::init();
-    }
-
-    public static function cookie()
-    {
-        static $loaded;
-
-        if (empty($loaded)) {
-            require_once(dirname(__FILE__) . '/cookie.php');
-
-            $loaded = true;
-        }
-
-        return Cookie::init();
-    }
-
-    public static function escape($output, $fromUrl = false)
-    {
-        $string = htmlspecialchars($output, ENT_COMPAT, 'UTF-8');
-
-        if ($fromUrl) {
-            $string = urlencode($string);
-        }
-
-        return $string;
-    }
+		return $string;
+	}
 }
 
 // Initiate session first
