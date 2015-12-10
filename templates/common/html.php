a7548738b5e0a8b9ba8b730e5252eacf4721a623
Jason Rey <jasonrey@outlook.com>
2015-12-10 14:27:10 +0800

    Fixed html template

--- templates/common/html.php
+++ templates/common/html.php
@@ -4,7 +4,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<base href="<?php echo Config::getHTMLBase(); ?>"
+	<base href="<?php echo Config::getHTMLBase(); ?>" />
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<meta charset="UTF-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no, minimal-ui" />
