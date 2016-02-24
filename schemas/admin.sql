54e440739644a86ba64cc010541da231f3aa45ad
Claire <claire@compass-interactive.com>
2016-02-24 17:53:08 +0800

    Updated create table sql

--- schemas/admin.sql
+++ schemas/admin.sql
@@ -3,8 +3,16 @@ CREATE TABLE `admin` (
   `username` varchar(255) NOT NULL DEFAULT '',
   `password` varchar(64) NOT NULL DEFAULT '',
   `salt` varchar(64) NOT NULL DEFAULT '',
-  `identifier` varchar(64) NOT NULL DEFAULT '',
   `lastlogin` datetime NOT NULL,
   `date` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `adminsession` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `admin_id` int(11) NOT NULL DEFAULT 0,
+  `identifier` varchar(64) NOT NULL DEFAULT '',
+  `date`  datetime NOT NULL,
+  `data` TEXT NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
