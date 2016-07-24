4ce2eab9d0c9a98c063a9dbc61e4cddc70a75364
Jason Rey <jasonrey@outlook.com>
2016-07-25 00:06:39 +0800

    Removed sample codes

--- index.php
+++ index.php
@@ -3,36 +3,4 @@
 
 require_once(dirname(__FILE__) . '/lib/lib.php');
 
-Lib::load('table');
-
-class ATable extends Table
-{
-	public static $tablename = 'a';
-	public $a;
-	public $a2;
-	public $a3;
-}
-
-class BTable extends Table
-{
-	public static $activedb = 'new';
-	public static $tablename = 'a';
-	public $a;
-	public $a2;
-	public $a3;
-}
-
-class CTable extends Table
-{
-	public static $tablename = 'c';
-	public $a;
-	public $a2;
-	public $a3;
-}
-
-$a = ATable::all();
-$b = BTable::all();
-
-var_dump($a, $b);
-
-// Lib::route();
+Lib::route();
