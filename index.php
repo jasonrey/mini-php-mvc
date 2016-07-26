0da24e73186d6877fb3077a926eff80ae592ffbf
Jason Rey <jasonrey@outlook.com>
2016-07-26 18:30:38 +0800

    Added composer support

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 !defined('SERVER_EXEC') && define('SERVER_EXEC', true);
 
-require_once(dirname(__FILE__) . '/lib/lib.php');
+require_once dirname(__FILE__) . '/lib/lib.php';
+
+require __DIR__ . '/vendor/autoload.php';
 
 Lib::route();
