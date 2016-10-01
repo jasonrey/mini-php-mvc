369c229c6f6dbb78987ec82dd1bad2fd17fde19e
Jason Rey <jasonrey@outlook.com>
2016-10-01 12:04:44 +0800

    Updated router build to obey segments regardless

--- router.php
+++ router.php
@@ -265,26 +265,18 @@ class Router
 				// If all segment match
 				if ($matched) {
 					if (is_callable($build['callback'])) {
-						$result = $build['callback']($data);
+						$build['callback']($data);
+					}
 
-						if ($result !== false && !empty($data)) {
-							$queryString = http_build_query($data);
+					$result = implode('/', $resultSegments);
 
-							if (!empty($queryString)) {
-								$result .= '?' . $queryString;
-							}
-						}
-					} else {
-						$result = implode('/', $resultSegments);
+					$remaining = array_diff_key($data, $usedKeys);
 
-						$remaining = array_diff_key($data, $usedKeys);
+					if (count($remaining) > 0) {
+						$queryString = http_build_query($remaining);
 
-						if (count($remaining) > 0) {
-							$queryString = http_build_query($remaining);
-
-							if (!empty($queryString)) {
-								$result .= '?' . $queryString;
-							}
+						if (!empty($queryString)) {
+							$result .= '?' . $queryString;
 						}
 					}
 				}
